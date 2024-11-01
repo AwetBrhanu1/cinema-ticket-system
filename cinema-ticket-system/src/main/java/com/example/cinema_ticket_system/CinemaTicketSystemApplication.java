3b0c5f95e6ab719873e8b25a51c0364010e62f61
AwetBrhanu1 <awetbrhanu122119@gmail.com>
2024-11-01 14:51:02 +0300

    first commit

---
+++ cinema-ticket-system/src/main/java/com/example/cinema_ticket_system/CinemaTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema_ticket_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketSystemApplication.class, args);
+	}
+
+}
---
+++ cinema-ticket-system/src/main/java/com/example/cinema_ticket_system/config/JwtService.java
@@ -0,0 +1,59 @@
+package com.example.cinema_ticket_system.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
+    public String generateToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 3000 * 60 * 60 * 30))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public String generateToken(String username) {
+        return generateToken(new HashMap<>(), username);
+    }
+    public String extractUserName(String token) {
+        // extract the username from jwt token
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+}
